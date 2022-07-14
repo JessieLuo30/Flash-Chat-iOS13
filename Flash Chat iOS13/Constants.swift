4333baa71d231f3533a26bda2e3bb869bc8d576c
Jessie Luo <jluo30@jhu.edu>
2022-07-13 18:02:48 -0700

    feat: set Segue, add constants file, install cocoa pods

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  constant.swift
+//  Flash Chat iOS13
+//
+//  Created by Jessie Luo on 7/13/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
